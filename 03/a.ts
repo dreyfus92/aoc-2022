7e7fc8cf15f364511b9007fc9527f6210cfa0355
Paul Valladares <85648028+dreyfus92@users.noreply.github.com>
2022-12-03 01:08:38 -0600

    third challenge completed

--- 03/a.ts
+++ 03/a.ts
@@ -3,4 +3,33 @@ const input = await Deno.readTextFile(
     p.fromFileUrl(import.meta.resolve("./input.txt")),
 );
 
+const lines = input.split("\n");
+
+const letterToNumber = (letter: string) => {
+    const alphabetLowerCaseandUpperCase = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    return alphabetLowerCaseandUpperCase.indexOf(letter) + 1;
+};
+
+function splitAnStringInHalves(str: string) {
+    const middle = Math.ceil(str.length / 2);
+    return [str.slice(0, middle), str.slice(middle)];
+}
+
+function getDuplicates(str: string) {
+    const [firstHalf, secondHalf] = splitAnStringInHalves(str);
+    const firstHalfArray = firstHalf.split("");
+    const secondHalfArray = secondHalf.split("");
+    const duplicates = firstHalfArray.filter((letter) => secondHalfArray.includes(letter));
+    return duplicates;
+}
+
+let totalSum = 0
+for (const line of lines) {
+    const duplicate = getDuplicates(line);
+    const number = letterToNumber(duplicate[0])
+    totalSum += number
+}
+
+console.log(totalSum)
+
 
--- 03/b.ts
+++ 03/b.ts
@@ -3,4 +3,37 @@ const input = await Deno.readTextFile(
     p.fromFileUrl(import.meta.resolve("./input.txt")),
 );
 
+const lines = input.split("\n");
+
+const letterToNumber = (letter: string) => {
+    const alphabetLowerCaseandUpperCase = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    return alphabetLowerCaseandUpperCase.indexOf(letter) + 1;
+};
+
+const groupLinesInGroupsOf3 = (lines: string[]) => {
+    const groups = [];
+    for (let i = 0; i < lines.length; i += 3) {
+        groups.push(lines.slice(i, i + 3));
+    }
+    return groups;
+}
+
+function getTriplicateInGroupOf3(group: string[]) {
+    const [firstLine, secondLine, thirdLine] = group;
+    const firstLineArray = firstLine.split("");
+    const secondLineArray = secondLine.split("");
+    const thirdLineArray = thirdLine.split("");
+    const triplicate = firstLineArray.filter((letter) => secondLineArray.includes(letter) && thirdLineArray.includes(letter));
+    return [...new Set(triplicate)];
+}
+
+let totalSum = 0
+
+for (const group of groupLinesInGroupsOf3(lines)) {
+    const triplicates = getTriplicateInGroupOf3(group)
+    const number = letterToNumber(triplicates[0])
+    totalSum += number
+}
+
+console.log(totalSum)
 
