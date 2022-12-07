2f496de688ef0d512722518f7435e90c89fd3e7f
Paul Valladares <85648028+dreyfus92@users.noreply.github.com>
2022-12-07 03:34:47 -0600

    added day 7 solution

--- 07/a.ts
+++ 07/a.ts
@@ -1,70 +1,67 @@
 import * as p from "https://deno.land/std@0.165.0/path/mod.ts";
 const input = await Deno.readTextFile(
-    p.fromFileUrl(import.meta.resolve("./inputTest.txt")),
+    p.fromFileUrl(import.meta.resolve("./input.txt")),
 );
 
-type Dir = {
-    parent?: Dir;
-    files: { [name: string]: number };
-    dirs: { [name: string]: Dir };
+let _id = 0;
+const id = () => {
+    _id++;
+    return _id;
 };
+type entry = { id: number; name: string; parent: number } & (
+    | { type: 'file'; fileSize: number }
+    | { type: 'dir' }
+);
+const entries: entry[] = [{ name: '/', type: 'dir', id: id(), parent: -1 }];
+let cwd = entries[0];
 
-const fs: Dir = { files: {}, dirs: {} };
-
-let current = fs;
-
-for (const command of input) {
-    const [cmd, ...rest] = command.split(" ");
-
-    if (cmd === "$") {
-        if (rest[0] === "cd") {
-            const dir = rest[1];
-            if (dir === "..") {
-                current = current.parent!;
-            } else if (dir === "/") {
-                current = fs;
+input
+    .split('\n')
+    .forEach((l) => {
+        const cmd = l.split(' ');
+        if (cmd[1] === 'cd') {
+            if (cmd[2] === '/') {
+                cwd = entries[0];
+            } else if (cmd[2] === '..') {
+                if (cwd.id === 1) return;
+                cwd = entries.find((e) => e.id === cwd.parent)!;
             } else {
-                if (!current.dirs[dir]) {
-                    current.dirs[dir] = { parent: current, files: {}, dirs: {} };
-                }
-                current = current.dirs[dir];
+                cwd = entries.find(
+                    (e) => e.parent === cwd.id && e.name === cmd[2]
+                )!;
             }
         }
-    } else if (cmd !== "dir") {
-        current.files[rest[0]] = parseInt(cmd);
-    }
-}
+        if (cmd[1] === 'ls') return;
+        if (cmd[0] === 'dir') {
+            const name = cmd[1];
+            entries.push({ name, type: 'dir', id: id(), parent: cwd.id });
+        }
+        if (/^\d+$/.test(cmd[0])) {
+            entries.push({
+                name: cmd[1],
+                type: 'file',
+                id: id(),
+                parent: cwd.id,
+                fileSize: Number(cmd[0])
+            });
+        }
+    });
 
-const dirSizes: number[] = [];
+const folderSize = (id: number): number =>
+    entries
+        .filter((e) => e.parent === id)
+        .reduce(
+            (a, e) => a + (e.type === 'file' ? e.fileSize : folderSize(e.id)),
+            0
+        );
 
-const computeFileSize = (curr: Dir): number => {
-    let size = 0;
+const sizes = entries
+    .filter((e) => e.type === 'dir')
+    .map((e) => folderSize(e.id));
 
-    for (const file in curr.files) {
-        size += curr.files[file];
-    }
+const A = sizes.filter((s) => s <= 100000).reduce((a, c) => a + c, 0);
+console.log(A);
 
-    for (const dir in curr.dirs) {
-        const dirSize = computeFileSize(curr.dirs[dir]);
-        size += dirSize;
-
-        dirSizes.push(dirSize);
-    }
-
-    return size;
-};
-
-computeFileSize(fs);
-
-const sumOfSub100k = dirSizes
-    .filter((x) => x <= 100000)
-    .reduce((a, b) => a + b);
-console.log("part 1:", sumOfSub100k);
-
-
-const rootSize = computeFileSize(fs);
-
-const smallestThatFixesSpace = Math.min(
-    ...dirSizes.filter((x) => x >= 30000000 - (70000000 - rootSize))
-);
-console.log("part 2:", smallestThatFixesSpace);
+const required = 30000000 - (70000000 - folderSize(1));
+const B = sizes.sort((a, b) => a - b).find((s) => s >= required);
+console.log(B);
