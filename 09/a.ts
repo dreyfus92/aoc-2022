6234d1a7af6ffcb7f52dc73ce64ec12e1385291f
Paul Valladares <85648028+dreyfus92@users.noreply.github.com>
2022-12-08 20:49:18 -0600

    added files for day 09

---
+++ 09/a.ts
@@ -0,0 +1,9 @@
+import * as p from "https://deno.land/std@0.165.0/path/mod.ts";
+const input = await Deno.readTextFile(
+    p.fromFileUrl(import.meta.resolve("./inputTest.txt")),
+);
+
+
+const lines = input.replaceAll("\r", "").split("\n");
+
+
